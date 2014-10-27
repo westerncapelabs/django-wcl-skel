600f82583600e1423b3979f6e8c70781653dbcc6
Adrian Moisey <adrian@changeover.za.net>
2014-10-27 19:07:28 +0200

    PEP8

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.app_name}}/tests.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/{{cookiecutter.app_name}}/tests.py
@@ -1,5 +1,4 @@
 import json
-from django.core.urlresolvers import reverse
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -8,7 +7,6 @@ from rest_framework.authtoken.models import Token
 
 from .models import *
 from .serializers import *
-
 
 
 class APITestCase(TestCase):
@@ -24,29 +22,31 @@ class AuthenticatedAPITestCase(APITestCase):
         self.username = 'testuser'
         self.password = 'testpass'
         self.user = User.objects.create_user(self.username,
-            'testuser@example.com', self.password)
+                                             'testuser@example.com',
+                                             self.password)
         token = Token.objects.create(user=self.user)
         self.token = token.key
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-
 
 
 class TestExampleAppHStore(AuthenticatedAPITestCase):
 
     def test_login(self):
         request = self.client.post(
-            '/{{cookiecutter.app_name}}/api-token-auth/', {"username": "testuser", "password": "testpass"})
+            '/{{cookiecutter.app_name}}/api-token-auth/',
+            {"username": "testuser", "password": "testpass"})
         token = request.data.get('token', None)
         self.assertIsNotNone(
             token, "Could not receive authentication token on login post.")
         self.assertEqual(request.status_code, 200,
-                         "Status code on /auth/login was %s (should be 200)." % request.status_code)
+                         "Status code on /auth/login was %s (should be 200)."
+                         % request.status_code)
 
     def test_create_dummy_model_data(self):
         post_data = {
             "msisdn": 1234,
             "product_code": "test_code",
-            "data": { 'a': 'a', 'b': 2 }
+            "data": {'a': 'a', 'b': 2}
         }
         response = self.client.post('/{{cookiecutter.app_name}}/dummy/',
                                     json.dumps(post_data),
@@ -57,5 +57,5 @@ class TestExampleAppHStore(AuthenticatedAPITestCase):
         d = DummyModel.objects.last()
         self.assertEqual(d.msisdn, 1234)
         self.assertEqual(d.product_code, 'test_code')
-        self.assertEqual(d.data, { 'a': 'a', 'b': '2' })
+        self.assertEqual(d.data, {'a': 'a', 'b': '2'})
 
