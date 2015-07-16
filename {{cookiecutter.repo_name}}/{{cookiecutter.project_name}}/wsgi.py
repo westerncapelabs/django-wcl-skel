647dc283d6edcac68664da2ecb8cac1e1fb86866
Mike Jones <mike@westerncapelabs.com>
2015-07-16 17:55:45 +0200

    Tweaks to issues

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "{{cookiecutter.project_name}}.settings")
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
